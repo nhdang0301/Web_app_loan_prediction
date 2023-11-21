ecc33b05d13fa2a54ad2f0f722dcf60a9cbb4087
nhdang0301 <ho66680@gmail.com>
2023-11-21 15:50:23 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -8,7 +8,7 @@ import matplotlib as plt
 from flask import Flask, render_template, redirect, url_for, request, flash
 
 # hello my friend
-# lau ngay qua roi nhen
+# hehehe chiu roiii
 app = Flask(__name__)
 app.secret_key = "NGUYENHAIDANG"
 scopes = ['https://www.googleapis.com/auth/spreadsheets',
